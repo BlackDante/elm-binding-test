85209dda17699878313af195cac05e288aa0cb61
Dante <dante@laptop-fhnh1j6t.home>
2016-07-27 23:36:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Elm binding test
+
+## Scope
+
+ - [x] Create form with inputs
+ - [x] Update Model on inputs change
+ - [x] Dispaly merge inputs data 
---
+++ form.elm
@@ -0,0 +1,53 @@
+module Main exposing (..)
+
+import Html exposing (div, form, input, span, text)
+import Html.App exposing (beginnerProgram)
+import Html.Events exposing (onInput)
+
+main : Program Never
+main =
+  beginnerProgram { model = model, view = view, update = update }
+
+
+type alias Model =
+  { firstname : String
+  , lastname : String
+  }
+
+
+model : Model
+model =
+  { firstname = ""
+  , lastname = ""
+  }
+
+
+type Msg = FirstNameUpdate String | LastNameUpdate String
+
+
+view : Model -> Html.Html Msg
+view model =
+  div []
+  [ form []
+    [ input [ onInput FirstNameUpdate ] []
+    , input [ onInput LastNameUpdate ] []
+    ]
+    , span [] [ text (name model) ]
+  ]
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    FirstNameUpdate content ->
+      { model |
+          firstname = content}
+
+    LastNameUpdate content ->
+      { model |
+          lastname = content }
+
+
+name : Model -> String
+name model =
+  "Fullname: " ++ model.firstname ++ " " ++ model.lastname
